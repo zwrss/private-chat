b6c6bd925ad2470b7f3ba435e2783d15621e39f2
pmlynarczyk <ppmlynarczyk@gmail.com>
2020-11-26 00:08:15 +0100

    Fun with console

--- console-client/build.sbt
+++ console-client/build.sbt
@@ -7,7 +7,8 @@ version := "0.1"
 scalaVersion := "2.13.3"
 
 libraryDependencies ++= Seq(
-  "com.typesafe.play" %% "play-json" % "2.9.1"
+  "com.typesafe.play" %% "play-json" % "2.9.1",
+  "org.fusesource.jansi" % "jansi" % "2.0.1"
 )
 
 enablePlugins(JavaAppPackaging)
--- console-client/src/main/scala/com/zwrss/privatechat/consoleclient/Client.scala
+++ console-client/src/main/scala/com/zwrss/privatechat/consoleclient/Client.scala
@@ -1,9 +1,9 @@
 package com.zwrss.privatechat.consoleclient
 
-import java.net.Socket
+import org.fusesource.jansi.Ansi
 
-import com.zwrss.privatechat.consoleclient.controller.SocketWrapper
-import com.zwrss.privatechat.message.SimpleMessage
+import scala.io.StdIn
+
 
 object Client {
 
@@ -12,14 +12,35 @@ object Client {
 
   def main(args: Array[String]): Unit = {
 
-    val socket = new SocketWrapper(new Socket(host, port))
-    socket write SimpleMessage("Hello!")
-    socket write SimpleMessage("This is a wut?")
-    socket write SimpleMessage("Bye!")
-    Thread sleep 10000
-    socket.close()
+    //    val socket = new SocketWrapper(new Socket(host, port))
+    //    socket write SimpleMessage("Hello!")
+    //    socket write SimpleMessage("This is a wut?")
+    //    socket write SimpleMessage("Bye!")
+    //    Thread sleep 10000
+    //    socket.close()
+    //
+    //    System exit 0
 
-    System exit 0
+    var buffer: String = ""
+
+    println("Last message: ")
+
+    val writingThread: Thread = new Thread {
+      override def run(): Unit = {
+        var i = 0
+        while (true) {
+          i += 1
+          print(Ansi.ansi.saveCursorPosition().cursorUpLine().eraseScreen(Ansi.Erase.BACKWARD).a(s"Last message $i $buffer").cursorDownLine().restoreCursorPosition())
+          Thread.sleep(500)
+        }
+      }
+    }
+    writingThread.start()
+
+    while (true) {
+      print("Me: ")
+      buffer = StdIn.readLine()
+    }
 
   }
 
